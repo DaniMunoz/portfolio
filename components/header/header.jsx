1b8b83a3a02ec717e07a911de301790e77f5a9f1
Dani <danielmunozgil@gmail.com>
2023-01-24 14:59:29 +0100

    Header language change added

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,10 +1,14 @@
+import { useSelector } from 'react-redux';
 import Link from "next/link";
 import Image from "next/image";
 import LanguageSelector from "../languageSelector/languageSelector";
-
-//import styles from "../dist/output.css";
+import languageData from '../../public/json/languages.json' assert { type: 'JSON' };
 
 export default function Header() {
+  //Obtain current language from redux store
+  const language = useSelector((state) => state.language.language);
+  const languageStrings = languageData[0][language];
+
   return (
     <header className="bg-amber-400 p-5 sticky top-0 flex flex-row">
       <Link className="basis-1/4" href="/">
@@ -13,9 +17,9 @@ export default function Header() {
       <nav className="text-xl md:text-2xl basis-3/4">
         <div className="flex flex-row-reverse">
           <LanguageSelector />&nbsp;&nbsp;&nbsp;
-          <Link href="/contact">Contact</Link>&nbsp;&nbsp;&nbsp;
-          <Link href="/qualifications">Qualifications</Link>&nbsp;&nbsp;&nbsp;
-          <Link href="/projects">Projects</Link>&nbsp;&nbsp;&nbsp;
+          <Link href="/contact">{languageStrings["header.contact"]}</Link>&nbsp;&nbsp;&nbsp;
+          <Link href="/qualifications">{languageStrings["header.qualifications"]}</Link>&nbsp;&nbsp;&nbsp;
+          <Link href="/projects">{languageStrings["header.projects"]}</Link>&nbsp;&nbsp;&nbsp;
         </div>
       </nav>
     </header>
